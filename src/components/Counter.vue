90798fc2e27e5dc5250ebfc283fe385147543609
Cindy Wong <cindywongdev@gmail.com>
2023-03-10 17:04:29 -0500

    added counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 @click="addOne">{{ counter }}</h1>
+        <h2>{{ cheese }}</h2>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue"
+
+export default {
+    name: "Counter",
+    props: ["cheese"],
+    setup(){
+        const counter = ref(0)
+        const addOne = () => counter.value += 1
+        return {counter, addOne}
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>

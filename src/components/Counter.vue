967a9deb9171810c445e71c6dceaf1f59ed4680f
Cindy Wong <cindywongdev@gmail.com>
2023-03-10 17:45:25 -0500

    added alt method

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,7 @@
     <div>
         <h1 @click="addOne">{{ counter }}</h1>
         <h2>{{ cheese }}</h2>
+        <h3 v-for="num of arr" :key="num">{{ num }}</h3>
     </div>
 </template>
 
@@ -11,11 +12,25 @@ import { ref } from "vue"
 export default {
     name: "Counter",
     props: ["cheese"],
+    // newer alt way using composition API
+    // resembles react hooks
     setup(){
         const counter = ref(0)
         const addOne = () => counter.value += 1
-        return {counter, addOne}
+        const arr = ref([1,2,3,4])
+        return {counter, addOne, arr}
     }
+    // og way using options API 
+    // data: function(){
+    //     return {
+    //         counter: 0
+    //     }
+    // },
+    // methods: {
+    //     addOne: function(){
+    //         this.counter += 1
+    //     }
+    // }
 }
 </script>
 
